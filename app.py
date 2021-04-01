825780d33d72650fe1dff40e31e643fab873d31b
Kristin Flores <kristin@Joan-Steppenbeck.local>
2021-03-31 23:17:09 -0500

    so much googling required

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/weather_app")
+
+# Route to render index.html template using data from Mongo
+@app.route("/")
+def home():
+
+    # Find one record of data from the mongo database
+    mars_info = mongo.db.collection.find_one()
+
+    # Return template and data
+    return render_template("index.html", mars_info=mars_info)
+
+
+# Route that will trigger the scrape function
+@app.route("/scrape")
+def scrape():
+
+    # Run the scrape function
+    mars_data = scrape_mars.scrape_mars_news()
+    mars_data = scrape_mars.scrape_feature_image()
+    mars_data = scrape_mars.scrape_mars_facts()
+    mars_data = scrape_mars.scrape_mars_imgs()
+
+    # Update the Mongo database using update and upsert=True
+    mongo.db.mars_info.update({}, mars_data, upsert=True)
+
+    # Redirect back to home page
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
--- scrape_mars.py
+++ scrape_mars.py
@@ -1,9 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# In[1]:
-
-
+# Import dependecies
 from splinter import Browser
 from bs4 import BeautifulSoup
 from webdriver_manager.chrome import ChromeDriverManager
@@ -11,179 +7,134 @@ import pandas as pd
 from urllib.request import urlopen
 
 
-# ## NASA Mars News
+#### NASA Mars News ####
 
-# In[2]:
+mars_info = {}
 
+def scrape_mars_news():
+    try:
 
-# browser = init_browser()
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
 
+        # browser = init_browser()
+        executable_path = {'executable_path': ChromeDriverManager().install()}
+        browser = Browser('chrome', **executable_path, headless=False)
 
-# In[3]:
 
+        url = "https://redplanetscience.com/"
+        browser.visit(url)
 
-url = "https://redplanetscience.com/"
-browser.visit(url)
+        html = browser.html
+        soup = BeautifulSoup(html, "html.parser")
 
-html = browser.html
-soup = BeautifulSoup(html, "html.parser")
+        news_title = soup.find('div', class_='content_title').text
+        news_p = soup.find('div', class_='article_teaser_body').text
 
-news_title = soup.find('div', class_='content_title').text
-news_p = soup.find('div', class_='article_teaser_body').text
+        # Dictionary entry from MARS NEWS
+        mars_info['news_title'] = news_title
+        mars_info['news_paragraph'] = news_p
 
-# news_data = {
-#         "news_title": news_title,
-#         "news_p": news_p,
-#     }
+        return mars_info
 
-# Return results
-print(news_title)
-print(news_p)
+    finally:
 
+        browser.quit()
 
-# In[4]:
 
+#### JPL Mars Space Images - Featured Image ####
 
-# Close the browser after scraping
-browser.quit()
+def scrape_feature_image():
+    try:
 
 
-# ## JPL Mars Space Images - Featured Image
+        executable_path = {'executable_path': ChromeDriverManager().install()}
+        browser = Browser('chrome', **executable_path, headless=False)
 
-# In[5]:
+        url = "https://spaceimages-mars.com/"
+        browser.visit(url)
 
+        browser.links.find_by_partial_text('FULL').click()
 
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
+        html = browser.html
+        soup = BeautifulSoup(html, "html.parser")
 
+        relative_image_path = soup.find('img', class_='fancybox-image')["src"]
+        featured_image_url = url + relative_image_path
 
-# In[6]:
+        # Dictionary entry from FEATURED IMAGE
+        mars_info['featured_image_url'] = featured_image_url 
+        
+        return mars_info
 
+    finally:
 
-url = "https://spaceimages-mars.com/"
-browser.visit(url)
+        browser.quit()
 
 
-# In[7]:
+#### Mars Facts ####
 
+def scrape_mars_facts():
+    try:
 
-browser.links.find_by_partial_text('FULL').click()
 
+        executable_path = {'executable_path': ChromeDriverManager().install()}
+        browser = Browser('chrome', **executable_path, headless=False)
 
-# In[8]:
+        url = 'https://galaxyfacts-mars.com/'
 
+        table = pd.read_html(url)
+        table
 
-html = browser.html
-soup = BeautifulSoup(html, "html.parser")
-
-
-# In[9]:
-
-
-relative_image_path = soup.find('img', class_='fancybox-image')["src"]
-featured_image_url = url + relative_image_path
-print(featured_image_url)
-
-
-# In[10]:
-
-
-# Close the browser after scraping
-browser.quit()
-
-
-# ## Mars Facts
-
-# In[26]:
-
-
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
-
-
-# In[27]:
-
-
-url = 'https://galaxyfacts-mars.com/'
-
-
-# In[28]:
-
-
-table = pd.read_html(url)
-table
-
-
-# In[29]:
-
-
-mars_data = table[0]
-mars_data.columns = ['Description', 'Mars', 'Earth']
-mars_data.set_index('Description', inplace=True)
-print(mars_data)
-
-
-# In[15]:
-
-
-# Close the browser after scraping
-browser.quit()
-
-
-# ## Mars Hemispheres
-
-# In[21]:
-
-
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
-
-
-# In[22]:
-
-
-url = 'https://marshemispheres.com/'
-browser.visit(url)
-
-
-# In[23]:
-
-
-html = browser.html
-soup = BeautifulSoup(html, "html.parser")
-
-section = soup.find_all('div', class_='item')
-
-image_urls = []
-
-for items in section:
-    # Finding title
-    title = items.find('h3').text
+        mars_data = table[0]
+        mars_data.columns = ['Description', 'Mars', 'Earth']
+        mars_data.set_index('Description', inplace=True)
     
-    # Find link to click for high res photoe, click link
-    href = items.find('a', class_='itemLink product-item')['href']
-    browser.visit('https://marshemispheres.com/' + href)
-    
-    # Parse this website and get final link
-    partial_href = browser.html
-    soup = BeautifulSoup(partial_href, 'html.parser')
-    image_link = 'https://marshemispheres.com/' + soup.find('img', class_='wide-image')['src']
+        # Dictionary entry from MARS DATA
+        mars_info['mars_data'] = mars_data
 
-    # Add title and final link to dictonary
-    image_urls.append({"title" : title, "image_link" : image_link})
+        return mars_info
+
+    finally:
+
+        browser.quit()
 
 
-# In[24]:
+#### Mars Hemisphere Images ####
+
+def scrape_mars_imgs():
+
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
+
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
 
 
-print(image_urls)
+    html = browser.html
+    soup = BeautifulSoup(html, "html.parser")
 
+    section = soup.find_all('div', class_='item')
 
-# In[20]:
+    image_urls = []
 
+    for items in section:
+        # Finding title
+        title = items.find('h3').text
+                
+        # Find link to click for high res photoe, click link
+        href = items.find('a', class_='itemLink product-item')['href']
+        browser.visit('https://marshemispheres.com/' + href)
+                
+        # Parse this website and get final link
+        partial_href = browser.html
+        soup = BeautifulSoup(partial_href, 'html.parser')
+        image_link = 'https://marshemispheres.com/' + soup.find('img', class_='wide-image')['src']
 
-# Close the browser after scraping
-browser.quit()
+        # Add title and final link to dictonary
+        image_urls.append({"title" : title, "image_link" : image_link})
+            
+    # Dictionary entry from MARS IMGS
+    mars_info['image_urls'] = image_urls
 
+    return mars_info
+
+    browser.quit()
